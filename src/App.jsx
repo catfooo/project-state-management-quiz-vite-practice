b8587341aff3bb351b62a9c2b20c64ae95b16f64
So Youn Choi <manghatuhyo@gmail.com>
2023-10-24 17:24:48 +0200

    showing options

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { CurrentQuestionZustand } from "./components/CurrentQuestionZustand";
+
+
+export const App = () => {
+  return (
+
+      <div>
+
+        <CurrentQuestionZustand />
+      </div>
+
+  );
+};
